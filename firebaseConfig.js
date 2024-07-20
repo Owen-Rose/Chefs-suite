218d1e87111466abf48b4138dab2b52face8e2c4
Owen-Rose <owen.rose@edu.sait.ca>
2024-07-20 15:48:07 -0600

    added firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-}
+  apiKey: "AIzaSyDYwrVUqa3MsF20uJ3ytBYnqP37mObP23w",
+  authDomain: "recipe-web-app-fdddb.firebaseapp.com",
+  projectId: "recipe-web-app-fdddb",
+  storageBucket: "recipe-web-app-fdddb.appspot.com",
+  messagingSenderId: "8910166849",
+  appId: "1:8910166849:web:2f5951a8d352d3a5e25fc0",
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
---
+++ models/User.ts
@@ -0,0 +1,22 @@
+// models/User.ts
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IUser extends Document {
+  email: string;
+  password: string;
+}
+
+const UserSchema: Schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.models.User ||
+  mongoose.model<IUser>("User", UserSchema);
